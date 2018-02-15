79460022ab6d7a375e6733b3b0a072abe8b9189c
mathilde lesauvage <lesauvage.mathilde94@gmail.com>
2018-02-15 17:05:35 +0100

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+
+  get 'doses/create'
+
+  get 'doses/destroy'
+
+  get 'cocktails/index'
+
+  get 'cocktails/show'
+
+  get 'cocktails/new'
+
+  get 'cocktails/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :cocktails
 end
+
